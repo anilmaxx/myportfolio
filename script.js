101fcbc85853ea998bd30537a5b34b7849e6400c
Anil kumar <anilkumarkilli29@gmail.com>
2026-04-07 11:34:43 +0530

    Add initial JavaScript and CSS files for responsive navigation menu and layout styling

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function toggleMenu(){
+    const menu=document.querySelector(".menulinks");
+    const icon=document.querySelector(".hamburgericon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+function toggleMenu(){
+    const menu=document.querySelector(".menulinks");
+    const icon=document.querySelector(".hamburgericon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
